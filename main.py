da2b172cea084d3797d08d109033b2468de2d8f4
erik <uniko152@gmail.com>
2020-12-05 15:55:37 +0400

    updated chat

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 
 USERS = {}
 
+
 def send(name, message):
     now = datetime.datetime.now()
     f = open("C:\\Users\\ernes\\Documents\\Code\\IntIdea codes\\1st-project\\Chat.txt", mode="a")
@@ -13,12 +14,14 @@ def send(name, message):
     f.close()
 
 
-
-def main():
+def config_load():
     global USERS
     config = open("C:\\Users\\ernes\\Documents\\Code\\IntIdea codes\\1st-project\\Config.json", mode="r")
     USERS = json.load(config)
 
+
+def chat_login():
+    global USERS
     print("Would you lake to register or you're registered already?")
     print("Press 1 to register, Press 2 if you're registered")
     choice = int(input())
@@ -26,8 +29,13 @@ def main():
         nickname = input("Enter your nickname: ")
         name = input("Enter your name: ")
         USERS[nickname] = name
+    else:
+        nickname = input("Enter your nickname: ")
+    return nickname
 
-    nickname = input("Enter your nickname: ")
+
+def chat_print(nickname):
+    global USERS
     while True:
         message = input()
         send(USERS[nickname], message)
@@ -35,9 +43,20 @@ def main():
             print("Goodbye")
             break
 
+
+def config_write():
+    global USERS
     f = open("C:\\Users\\ernes\\Documents\\Code\\IntIdea codes\\1st-project\\Config.json", mode="w")
     json.dump(USERS, f)
     f.close()
+
+
+def main():
+    config_load()
+    nickname = chat_login()
+    chat_print(nickname)
+    config_write()
+
 
 if __name__ == '__main__':
     main()
